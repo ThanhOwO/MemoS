83ab760695672351240ad22090c7251d7d381fed
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2023-06-04 11:39:09 +0700

    add function edit and delete post for user

--- app/src/main/java/com/Thanh/memos/adapter/UserAdapter.java
+++ app/src/main/java/com/Thanh/memos/adapter/UserAdapter.java
@@ -49,7 +49,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
 
         Glide.with(holder.itemView.getContext().getApplicationContext())
                 .load(list.get(position).getProfileImage())
-                .placeholder(R.drawable.ic_person)
+                .placeholder(R.drawable.profile)
                 .timeout(6500)
                 .into(holder.profileImage);
 
