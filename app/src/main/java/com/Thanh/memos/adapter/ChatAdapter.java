b2a6dc7cd2cd5e3bbe32afd42350a1925381d99f
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2023-06-05 21:08:56 +0700

    fix UI

--- app/src/main/java/com/Thanh/memos/adapter/ChatAdapter.java
+++ app/src/main/java/com/Thanh/memos/adapter/ChatAdapter.java
@@ -1,6 +1,7 @@
 package com.Thanh.memos.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.Thanh.memos.R;
 import com.Thanh.memos.model.ChatModel;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatHolder> {
     Context context;
@@ -44,22 +50,38 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatHolder> {
             holder.leftchat.setVisibility(View.GONE);
             holder.rightchat.setVisibility(View.VISIBLE);
             holder.rightchat.setText(list.get(position).getMessage());
+            holder.profileImg.setVisibility(View.GONE);
         }else{
             holder.rightchat.setVisibility(View.GONE);
             holder.leftchat.setVisibility(View.VISIBLE);
             holder.leftchat.setText(list.get(position).getMessage());
+
+            FirebaseFirestore.getInstance().collection("Users")
+                    .document(list.get(position).getSenderID())
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document != null && document.exists()) {
+                                String profileImage = document.getString("profileImage");
+                                Glide.with(context).load(profileImage).into(holder.profileImg);
+                            }
+                        }
+                    });
         }
     }
 
     static class ChatHolder extends RecyclerView.ViewHolder{
 
         TextView leftchat, rightchat;
+        CircleImageView profileImg;
 
         public ChatHolder(@NonNull View itemView) {
             super(itemView);
 
             leftchat = itemView.findViewById(R.id.leftChat);
             rightchat = itemView.findViewById(R.id.rightChat);
+            profileImg = itemView.findViewById(R.id.profileImage);
         }
     }
 }
